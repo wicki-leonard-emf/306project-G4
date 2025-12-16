8333a87e206731cae74c823ab489e2e302eb6c2a
wicki-leonard-emf <leonard.wicki@studentfr.ch>
2025-12-16 10:27:55 +0100

    commitos commico comos come on cabron

--- code/front/vite.config.ts
+++ code/front/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '',
+  base: '/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
