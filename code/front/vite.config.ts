2a65cc1a872299bef810c01c6aa9cae4b2dc7d5e
wicki-leonard-emf <leonard.wicki@studentfr.ch>
2025-12-16 09:42:26 +0100

    commitos

--- code/front/vite.config.ts
+++ code/front/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
+  base: '',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
