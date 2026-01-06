57a6e43a157d8e966012445c66d635d7088d0da6
wicki-leonard-emf <leonard.wicki@studentfr.ch>
2026-01-06 09:15:50 +0100

    Add toast notifications for subscription management and room updates

--- code/front/src/main.tsx
+++ code/front/src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { AppRouter } from "./AppRouter.tsx";
+import { Toaster } from "sonner";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<AppRouter />);
+createRoot(document.getElementById("root")!).render(
+    <>
+        <AppRouter />
+        <Toaster position="top-right" richColors />
+    </>
+);
